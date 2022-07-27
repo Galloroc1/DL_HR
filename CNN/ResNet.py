b59879d144de6b1f0f7c729522a8f5cd70c6013b
Galloroc <heran@8lab.cn>
2022-07-27 15:12:24 +0800

    add vgg Bottleneck and fix some bug

--- CNN/ResNet.py
+++ CNN/ResNet.py
@@ -1,33 +1,33 @@
 import torch
 import torch.nn as nn
-from .core import BaseCNN
+from .core import BaseCNN, LAYER_PARAMS_DICT
 
 
-class ResNet(nn.Module, BaseCNN):
+class ResNet(BaseCNN):
     # conv [output_channel,kernel_size,stride,padding]
     # pool [kernel_size,stride,padding]
     HEAD_LAYER = {"conv1": [64, 7, 2, 3], "pool1": [3, 2, 1]}
     # input_channel out_channel
-    LAYER_PARAMS = {18: [{"BasicBlock": [[64, 64], [64, 64]],'downsample': False},
-                         {"BasicBlock": [[64, 128], [128, 128]],'downsample': True},
-                         {"BasicBlock": [[128, 256], [256, 256]],'downsample': True},
-                         {"BasicBlock": [[256, 512], [512, 512]],'downsample': True},
-                         ]}
     # fc layer input channel and out channel
-    FC_PARAMS = {"in_channel": 512, "out_channel": 1000}
     # todo ：add BasicBlock
+    FC_PARAMS = {18: {"in_channel": 512, "out_channel": 1000}, 34: {"in_channel": 512, "out_channel": 1000},
+                 50: {"in_channel": 2048, "out_channel": 1000}, 101: {"in_channel": 2048, "out_channel": 1000},
+                 152: {"in_channel": 2048, "out_channel": 1000}, }
     BASICBLOCK = [18, 34]
+    BOTTLENECK = [50,101,152]
+    NAME = 'resnet'
 
     def __init__(self, depth: int = 18):
         """
         :param depth: you can choose resnet 18 or other
         """
-        nn.Module.__init__(self)
-        BaseCNN.__init__(self)
-        assert depth in self.LAYER_PARAMS.keys(),f'not support depth ,we just support {self.LAYER_PARAMS.keys()}'
-        self.depth = depth
+        assert depth in LAYER_PARAMS_DICT[
+            self.NAME].keys(), f'not support depth ,we just support {LAYER_PARAMS_DICT[self.NAME].keys()}'
+        super(ResNet, self).__init__(depth=depth)
+        self.layer_params = LAYER_PARAMS_DICT[self.NAME][self.depth]
         self.avg_pool = torch.nn.AvgPool2d(kernel_size=7)
-        self.fc = torch.nn.Linear(self.FC_PARAMS["in_channel"],self.FC_PARAMS['out_channel'])
+        self.fc = torch.nn.Linear(self.FC_PARAMS[depth]["in_channel"], self.FC_PARAMS[depth]['out_channel'])
+        self.build_model()
 
     def build_model(self):
         # build head
@@ -39,17 +39,25 @@ class ResNet(nn.Module, BaseCNN):
                 self.sequential.add_module(k, torch.nn.MaxPool2d(kernel_size=v[0], stride=v[1], padding=v[2]))
 
         if self.depth in self.BASICBLOCK:
-            for k, v in enumerate(self.LAYER_PARAMS[self.depth]):
-                for index,item in enumerate(v['BasicBlock']):
+            for k, v in enumerate(self.layer_params):
+                for index, item in enumerate(v['BasicBlock']):
                     self.sequential.add_module("BasicBlock_Group" + str(k) + str(index),
                                                BasicBlock(item[0], item[1],
-                                                          downsample=v['downsample']))
-                    v['downsample'] = False if v['downsample'] else v['downsample']
+                                                          down_sample=v['down_sample']))
+                    v['down_sample'] = False if v['down_sample'] else v['down_sample']
+        else:
+            for k, v in enumerate(self.layer_params):
+                for index, item in enumerate(v['BasicBlock']):
+                    self.sequential.add_module("BasicBlock_Group" + str(k) + str(index),
+                                               Bottleneck(item[0], item[1],item[2],
+                                                          down_sample=v['down_sample'],down_sample_size=v['down_sample_stride']))
+                    v['down_sample'] = False if v['down_sample'] else v['down_sample']
+                    v['down_sample_stride'] = 1 if v['down_sample'] else v['down_sample_stride']
 
     def forward(self, x):
         x = self.sequential(x)
         x = self.avg_pool(x)
-        x = torch.flatten(x,1)
+        x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
 
@@ -58,9 +66,10 @@ class BasicBlock(nn.Module):
     """
     basicblock
     """
-    def __init__(self, input_channel: int = 64, output_channel: int = 64, downsample: bool = False):
+
+    def __init__(self, input_channel: int = 64, output_channel: int = 64, down_sample: bool = False):
         super(BasicBlock, self).__init__()
-        stride = 2 if downsample else 1
+        stride = 2 if down_sample else 1
         self.conv1 = torch.nn.Conv2d(in_channels=input_channel, out_channels=output_channel, kernel_size=3,
                                      stride=stride, padding=1)
         self.bn1 = torch.nn.BatchNorm2d(output_channel)
@@ -69,25 +78,63 @@ class BasicBlock(nn.Module):
                                      padding=1)
         self.bn2 = torch.nn.BatchNorm2d(output_channel)
         self.activation2 = torch.nn.ReLU()
-        self.downsample_layer = torch.nn.Conv2d(in_channels=input_channel, out_channels=output_channel, kernel_size=1,
-                                                stride=2, padding=0)
-        self.downsample = downsample
+        if down_sample:
+            self.down_sample_layer = torch.nn.Conv2d(in_channels=input_channel, out_channels=output_channel, kernel_size=1,
+                                                     stride=stride, padding=0)
+        self.down_sample = down_sample
 
-    def forward(self, x:torch.Tensor)->torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         :param x: forward data,must be tensor
         :return:basic_block forward result
         """
         identity = x
-        # if not downsample,just add x
-        identity = self.downsample_layer(identity) if self.downsample else identity
+        # if not down_sample,just add x
+        identity = self.down_sample_layer(identity) if self.down_sample else identity
         # todo : why bn before conv or after conv
         x = self.activation1(self.bn1(self.conv1(x)))
         x = self.activation2(self.bn2(self.conv2(x)) + identity)
         return x
 
 
-class Bottleneck(nn.Module, BaseCNN):
-    def __init__(self):
-        Bottleneck.__init__(self)
-        BaseCNN.__init__(self)
+class Bottleneck(nn.Module):
+    def __init__(self, input_channel: int = 64,mid_channel:int = 64 ,output_channel: int = 64, down_sample: bool = False,
+                 down_sample_size:int = 1):
+        super(Bottleneck, self).__init__()
+        if down_sample:
+            self.down_sample_layer = torch.nn.Conv2d(in_channels=input_channel, out_channels=output_channel, kernel_size=1,
+                                                     stride=down_sample_size, padding=0)
+        # layer1
+        self.conv1 = torch.nn.Conv2d(in_channels=input_channel, out_channels=mid_channel, kernel_size=1,
+                                     stride=1, padding=0,bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(mid_channel)
+
+        # layer2
+        self.conv2 = torch.nn.Conv2d(in_channels=mid_channel, out_channels=mid_channel, kernel_size=3, stride= down_sample_size,
+                                     padding=1,bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(mid_channel)
+
+        # layer3
+        self.conv3 = torch.nn.Conv2d(in_channels=mid_channel, out_channels=output_channel, kernel_size=1, stride=1,
+                                     padding=0,bias=False)
+        self.bn3 = torch.nn.BatchNorm2d(output_channel)
+        self.activation = torch.nn.ReLU()
+
+        # down_sample
+
+        self.down_sample = down_sample
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: forward data,must be tensor
+        :return:basic_block forward result
+        """
+        identity = x
+        # if not down_sample,just add x
+        identity = self.down_sample_layer(identity) if self.down_sample else identity
+        # todo : why bn before conv or after conv
+        x = self.bn1(self.conv1(x))
+        x = self.bn2(self.conv2(x))
+        x = self.activation(self.bn3(self.conv3(x)) + identity)
+        return x
+
--- CNN/VGG.py
+++ CNN/VGG.py
@@ -1,34 +1,18 @@
 import torch
-import torch.nn as nn
-from .core import BaseCNN
-from uitl import check_params_type_int
+from .core import BaseCNN, LAYER_PARAMS_DICT
 
 
-class VGG(nn.Module, BaseCNN):
-    LAYER_PARAMS = {
-        11: {"conv1": [64], "pool1": 'M', "conv2": [128], "pool2": "M", "conv3": [256, 256], "pool3": "M",
-             "conv4": [512, 512], "pool4": "M", "conv5": [512, 512], "pool5": "M"},
-        13: {"conv1": [64, 64], "pool1": 'M', "conv2": [128, 128], "pool2": "M", "conv3": [256, 256], "pool3": "M",
-             "conv4": [512, 512], "pool4": "M", "conv5": [512, 512], "pool5": "M"},
-        16: {"conv1": [64, 64], "pool1": 'M', "conv2": [128, 128], "pool2": "M", "conv3": [256, 256, 256], "pool3": "M",
-             "conv4": [512, 512, 512], "pool4": "M", "conv5": [512, 512, 512], "pool5": "M"},
-        19: {"conv1": [64, 64], "pool1": 'M', "conv2": [128, 128], "pool2": "M", "conv3": [256, 256, 256, 256],
-             "pool3": "M", "conv4": [512, 512, 512, 512], "pool4": "M", "conv5": [512, 512, 512, 512],
-             "pool5": "M"},
-    }
+class VGG(BaseCNN):
+    NAME = 'vgg'
 
-    def __init__(self, depth: int = 16,) -> None:
-
-        nn.Module.__init__(self)
-        BaseCNN.__init__(self)
-        # params check
-        assert depth in self.LAYER_PARAMS.keys(), 'not support depth'
-        # init sequential
-        self.depth = depth
+    def __init__(self, depth: int = 16, ) -> None:
+        assert depth in LAYER_PARAMS_DICT[self.NAME].keys(), 'not support depth'
+        super(VGG, self).__init__(depth=depth)
+        self.layer_params = LAYER_PARAMS_DICT[self.NAME][self.depth]
 
     def build_model(self) -> None:
         last_channel = self.input_channel
-        for k, v in self.LAYER_PARAMS[self.depth].items():
+        for k, v in self.layer_params.items():
             if k[0:4] == 'pool':
                 self.sequential.add_module(k, torch.nn.MaxPool2d(kernel_size=2))
             else:
@@ -43,4 +27,3 @@ class VGG(nn.Module, BaseCNN):
     def forward(self, x):
         r = self.sequential(x)
         return r
-
