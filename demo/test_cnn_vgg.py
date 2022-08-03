d41449b5304308cb70e1c008e32b579da0a59b15
1176651368 <48621533+1176651368@users.noreply.github.com>
2022-08-03 09:34:24 +0800

    add : u-net base conv module

--- demo/test_cnn_vgg.py
+++ demo/test_cnn_vgg.py
@@ -1,5 +1,8 @@
 # todo add root path
 from CNN.VGG import VGG
 from torchvision.models.vgg import vgg16
+
 r = VGG(depth=16)
-r.detail(is_print=True)
+# r.load_state_dict_(drop_key_lens=3)
+
+print(r)
